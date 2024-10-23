eb12f9d64db5f0cf889c6f8b158c63de3e03ae63
aleruizrg <aleruiz@alumno.unlam.edu.ar>
2024-10-23 20:00:05 -0300

    subida de proyecto Rekognition

---
+++ Rekognition.Demo/Controllers/RekognitionController.cs
@@ -0,0 +1,44 @@
+﻿using Amazon.Rekognition.Model;
+using Amazon.Rekognition;
+using Microsoft.AspNetCore.Mvc;
+using Rekognition.Entidades;
+
+namespace Rekognition.Demo.Controllers
+{
+    public class RekognitionController : Controller
+    {
+        private readonly IAmazonRekognition _rekognitionClient;
+
+        public RekognitionController(IAmazonRekognition rekognitionClient)
+        {
+            _rekognitionClient = rekognitionClient;
+        }
+
+        [HttpPost("detect-labels")]
+        public async Task<IActionResult> DetectLabels(IFormFile file)
+        {
+            using var memStream = new MemoryStream();
+            file.CopyTo(memStream);
+            var response = await _rekognitionClient.DetectLabelsAsync(new DetectLabelsRequest
+            {
+                Image = new Amazon.Rekognition.Model.Image
+                {
+                    Bytes = memStream
+                },
+                MinConfidence = 95
+            });
+
+            var labelResults = response.Labels.Select(label => new LabelResult
+            {
+                Name = label.Name,
+                Confidence = label.Confidence,
+                Parents = label.Parents.Select(p => p.Name).ToList(),
+                Aliases = label.Aliases.Select(a => new Entidades.LabelAlias { Name = a.Name }).ToList(),
+                Categories = label.Categories.Select(c => new Entidades.LabelCategory { Name = c.Name }).ToList(),
+                
+            }).ToList();
+
+            return Ok(labelResults);
+        }
+    }
+}
---
+++ Rekognition.Entidades/LabelResult.cs
@@ -0,0 +1,21 @@
+﻿namespace Rekognition.Entidades
+{
+    public class LabelAlias
+    {
+        public string Name { get; set; }
+    }
+
+    public class LabelCategory
+    {
+        public string Name { get; set; }
+    }
+
+    public class LabelResult
+    {
+        public string Name { get; set; }
+        public float Confidence { get; set; }
+        public List<string> Parents { get; set; }
+        public List<LabelAlias> Aliases { get; set; }
+        public List<LabelCategory> Categories { get; set; }
+    }
+}
